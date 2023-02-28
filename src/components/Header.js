0d7e376a9eafa27e573eb2f0ae417f92eaf54130
lexeach <toaddenterprises@gmail.com>
2023-02-28 19:20:42 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ function DrawerAppBar(props) {
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
-        ICO
+        Solarian Coin
       </Typography>
       <Divider />
       <List>
